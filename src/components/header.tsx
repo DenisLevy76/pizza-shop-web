2a404ce2b4a013ae87aaa9d362f9d45c0d95ab93
DenisLevy76 <denis.levy11@gmail.com>
2024-05-29 22:28:30 -0300

    feat: header style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 import Link from 'next/link'
 
 import { ThemeToggle } from './theme-toggle'
@@ -6,26 +7,21 @@ import { Button } from './ui/button'
 export const Header: React.FC = () => {
   return (
     <header className="flex items-center justify-between border-b border-foreground/20 bg-background px-8 py-2">
+      <Pizza />
       <nav>
         <ul className="flex gap-4">
           <li>
             <Button variant="ghost" asChild>
-              <Link href="#">Performance</Link>
+              <Link href="#" className="space-x-2">
+                <Home className="h-4 w-4" /> <span>Inicio</span>
+              </Link>
             </Button>
           </li>
           <li>
             <Button variant="ghost" asChild>
-              <Link href="#">Catalogo</Link>
-            </Button>
-          </li>
-          <li>
-            <Button variant="ghost" asChild>
-              <Link href="/clients">Clientes</Link>
-            </Button>
-          </li>
-          <li>
-            <Button variant="ghost" asChild>
-              <Link href="/sales">Vendas</Link>
+              <Link href="#" className="space-x-2">
+                <UtensilsCrossed className="h-4 w-4" /> <span>Pedidos</span>
+              </Link>
             </Button>
           </li>
         </ul>
